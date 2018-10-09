1a58001f01ec11f9a80785887fa9a95e49bb344b
Leonardo Andrés Garcia Crespo <leoasis@gmail.com>
2018-10-09 12:56:33 +0100

    Update example to use CRA 2.0

--- examples/basic/src/App.js
+++ examples/basic/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag.macro';
-import Friend from './Friend';
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag.macro";
+import Friend from "./Friend";
 
 const HERO_QUERY = gql`
   query GetCharacter($episode: Episode!) {
@@ -18,40 +18,35 @@ const HERO_QUERY = gql`
   ${Friend.fragments.friend}
 `;
 
-const withData = graphql(HERO_QUERY, {
-  options: ({ episode }) => ({
-    variables: { episode },
-  }),
-  props: ({ data }) => ({ ...data }),
-});
+const Character = ({ episode }) => (
+  <Query query={HERO_QUERY} variables={{ episode }}>
+    {({ loading, data, error }) => {
+      if (loading) return <div>Loading</div>;
+      if (error) return <h1>ERROR</h1>;
+      const { hero } = data;
 
-const CharacterWithoutData = ({ loading, hero, error }) => {
-  if (loading) return <div>Loading</div>;
-  if (error) return <h1>ERROR</h1>;
-  return (
-    <div>
-      {hero &&
+      return (
         <div>
-          <h3>
-            {hero.name}
-          </h3>
+          {hero && (
+            <div>
+              <h3>{hero.name}</h3>
 
-          {hero.friends &&
-            hero.friends.map(
-              friend =>
-                friend &&
-                <Friend key={friend.id} friend={friend} />
-            )}
-        </div>}
-    </div>
-  );
-};
+              {hero.friends &&
+                hero.friends.map(
+                  friend => friend && <Friend key={friend.id} friend={friend} />
+                )}
+            </div>
+          )}
+        </div>
+      );
+    }}
+  </Query>
+);
 
-const Character = withData(CharacterWithoutData);
-
-const App = () =>
+const App = () => (
   <div>
     <Character episode="NEWHOPE" />
-  </div>;
+  </div>
+);
 
 export default App;
--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloClient, createNetworkInterface } from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import App from "./App";
 
 const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: 'https://mpjk0plp9.lp.gql.zone/graphql',
-  }),
+  uri: "https://mpjk0plp9.lp.gql.zone/graphql"
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
